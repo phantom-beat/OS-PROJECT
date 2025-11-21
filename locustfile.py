ff7c9004c8ab54069613f22935ca9705acd83181
phantom-beat <mijodas0705@gmail.com>
2025-11-20 22:49:03 -0500

    a

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,9 @@
 from locust import HttpUser, task, between
 
 class WebsiteUser(HttpUser):
+    # Especifica el host del servidor web a probar
+    host = "http://192.168.56.101"
+
     wait_time = between(1, 5)
 
     @task
